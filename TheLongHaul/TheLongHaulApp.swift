12bf57c089dfacfa056e46268d6d635258ab7e34
Steven Yau <steve.cyau@gmail.com>
2026-02-20 09:30:55 -0800

    Initial Commit

---
+++ TheLongHaul/TheLongHaulApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLongHaulApp.swift
+//  TheLongHaul
+//
+//  Created by Steven Yau on 2026-02-20.
+//
+
+import SwiftUI
+
+@main
+struct TheLongHaulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
